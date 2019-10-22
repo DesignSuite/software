12b4d92e18b4fec3fafba2c7d3a05000491db492
Heijns <>
2019-10-22 10:17:07 -0400

    Update InstallCreativeApps.sh

--- Ubuntu/InstallCreativeApps.sh
+++ Ubuntu/InstallCreativeApps.sh
@@ -5,6 +5,7 @@ snap install gimp
 snap install inkscape
 snap install libreoffice
 snap install freecad
+snap install scribus
 
 wget https://cdn.lwks.com/lightworks-14.5.0-amd64.deb
 dpkg -i lightworks-14.5.0-amd64.deb
